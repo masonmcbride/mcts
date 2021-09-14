3a644671218e484cbb5c015f1589ae51ef717fa4
Mason McBride <mason1@berkeley.edu>
2021-09-13 21:33:20 -0700

    wrote others and more for search

--- mcts_search.py
+++ mcts_search.py
@@ -1,13 +1,15 @@
 class MCTS:
     def __init__(self, node):
         #I'm just pointing to a node now
-        self.node_pointer = node
+        self.node_pointer: MonteCarloTreeSearchNode = node
 
+    #returns MonteCarloTreeSearchNode with highest Q value. 
     def best_action(self, num_simulations):
         self.search(num_simulations)
         best: MonteCarloTreeSearchNode = self.node_pointer.best_child(c_param=0.)
         return best
 
+    #select and expand of mcts
     def _tree_policy(self):
         current_node = self.node_pointer
         while not current_node.is_terminal_node():
@@ -18,14 +20,20 @@ class MCTS:
 
         return current_node
 
+    #rollout and backprop of mcts. This is one iteration of the algorithm
     def run(self):
         v = self._tree_policy()
         reward = v.rollout()
         v.backpropagate(reward)
 
+    #searches the node for num_simulations
     def search(self, num_simulations):
         for _ in range(num_simulations):
             self.run()
+
+    #this function should process each node to prune and abstract if possible
+    def think(self):
+        pass
 
 
 """"
@@ -37,4 +45,4 @@ if best.n / simulations_number >= .99:
 new addition: this is a crude pure strategy detector
 if it is a pure strategy, prune tree by skipping node
 TODO Is it possible for the tree to be fine with multiple players being on the same depth n of the node?
-    """
+"""
